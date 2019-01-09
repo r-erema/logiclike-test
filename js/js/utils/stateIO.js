f03bfe3e9b052e80a8cef9c1f509581299aa7e4e
r.erema <roma.a.erema@gmail.com>
2019-01-09 16:28:40 +0300

    Refactoring

--- js/js/utils/stateIO.js
+++ js/js/utils/stateIO.js
@@ -2,7 +2,7 @@ export const loadState = () => {
     try {
         const serializedState = localStorage.getItem('state');
         if (serializedState === null) {
-            return undefined || {contacts: []};
+            return {contacts: []};
         }
         return JSON.parse(serializedState);
     } catch (e) {
